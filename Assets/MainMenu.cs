17e21362990c32b1696340ef47ef74e595a6c689
0xCAFFEEBABA <katerinakipapadopoulou24@gmail.com>
2020-08-05 21:18:50 +0300

    maybe better this time
    change pngs to text for sure

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Gets the index of the current scene and increases it by one
+        // In order to load the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
