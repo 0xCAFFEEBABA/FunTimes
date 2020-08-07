2d67b81958295963b147540f5c0e42d2dcb2ab31
0xCAFFEEBABA <katerinakipapadopoulou24@gmail.com>
2020-08-07 17:24:00 +0300

    still tryin'
    changed to navigate scenes and done for the 2 and did some other stuff too

---
+++ Assets/Codes/NavigateScenes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigateScenes : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("cardGame");
+    }
+
+    public void ChooseDecks()
+    {
+        SceneManager.LoadScene("chooseDecks");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+}
