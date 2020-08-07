07aabe0ad60d9d179e73c5a459a969af1f0f00ce
0xCAFFEEBABA <katerinakipapadopoulou24@gmail.com>
2020-08-07 23:14:23 +0300

    we are ok now

---
+++ Assets/Codes/LanguageSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LanguageSettings : MonoBehaviour
+{
+
+    public void ChangeImage(Sprite image)
+    {
+        this.GetComponent<Button>().image.sprite = image;
+    }
+
+}
